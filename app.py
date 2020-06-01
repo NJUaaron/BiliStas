bd04d320c1fcb400b2a6dfbd83acc47bd9aeed38
Aaron Leo <aaronxyliu@qq.com>
2020-06-01 19:19:26 +0800

    update

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def login():
         phone = request.form.get('phone')
         password = request.form.get('password')
         print(phone, password)
-        return redirect('base')
+        return redirect('basic')
     return render_template('login.html', form=login_form)
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -25,9 +25,21 @@ def Register():
     register_form = RegisterForm()
     return render_template('register.html', form=register_form)
 
-@app.route('/base')
-def Base():
-    return render_template('base.html')
+@app.route('/basic')
+def Basic():
+    return render_template('basic.html')
+
+@app.route('/fansfluct')
+def FansFluct():
+    return render_template('fansfluct.html')
+
+@app.route('/activedata')
+def ActiveData():
+    return render_template('activedata.html')
+
+@app.route('/category')
+def Category():
+    return render_template('category.html')
 
 
 if __name__ == '__main__':
